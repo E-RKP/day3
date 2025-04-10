c1c5ef6ab67a38c3fc13cb6a984aba8743c03740
E-RKP <edobackup111@gmail.com>
2025-04-10 17:04:49 +0200

    un sacco di robe in più

--- datatype.js
+++ datatype.js
@@ -74,6 +74,9 @@ console.log (completo)
 let somma = '56'+'56'
 console.log(Number(somma)+1)
 console.log(typeof somma)
+console.log(Number(somma))
+let element = `${(Number(somma))+ (Number(somma))}`
+console.log(element)
 
 let ds = 56
 let lkj = '11'
@@ -85,4 +88,105 @@ let gty = 'ciao a tutti '
 let rrr = 77
 let elemento = `${gty}${rrr.toString()}`
 
-console.log (elemento)
+console.log(elemento)
+
+let aaa = 2
+let bbb = 3
+let risultatotone = `${aaa} è maggiore di ${bbb}: ${aaa > bbb}`
+console.log(risultatotone)
+
+
+let spiru = 'spuffa'
+let spirulo = 'spuffo'
+let lungo = `${(spirulo.length) === (spiru.length)}`
+console.log(lungo)
+
+let gabibbo = 'gabibbo'
+console.log(gabibbo.search('i'))
+let firstletter = gabibbo[0]
+console.log(firstletter)
+
+let string = 'Javascript'
+console.log(string.substring(0,4))
+
+
+
+
+
+
+let uU='You cannot end a sentence with because because because is a conjunction'
+let bb ='   30 days of Challenge                      '
+let declare = '30 days of Challenge'
+console.log(declare)
+console.log(declare.length)
+console.log(declare.toUpperCase())
+console.log(declare.toLowerCase())
+console.log(declare.substring(4))
+console.log(declare.substring(0,3))
+console.log(declare.includes('script'))
+console.log(declare.split())
+console.log(declare.split(' '))
+let exercise = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(exercise.split(','))
+console.log(declare.replace('Challenge', 'Python'))
+console.log(declare.charAt(15))
+console.log(declare.charCodeAt(15))
+console.log(declare.indexOf('a'))
+console.log(uU.search('because'))
+console.log(declare.trim())
+console.log(declare.startsWith('30'))
+console.log(declare.startsWith('days'))
+console.log(declare.endsWith('enge'))
+console.log(declare.repeat(2))
+console.log(declare.concat(' minghie'))
+
+let word = '"There is no exercise better for the heart than reaching down and lifting people up."'
+console.log(word.concat(' By John Holmes teaches us to help one another.'))
+let words = "'Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same'"
+console.log(words)
+
+let minchia = 10
+console.log(typeof minchia === typeof 10)
+
+let nove = '9.8'
+console.log(nove == 10)
+console.log(typeof Number(nove) == typeof 10)
+let novaccio = Math.round(nove)
+console.log(novaccio == 10)
+
+let python ='phyton'
+let jargon ='jargon'
+
+console.log(python.includes('on'))
+console.log(jargon.includes('on'))
+let ooo = `${python.includes('on') == jargon.includes('ja')}`
+console.log(ooo)
+let uff = 'I hope this course is not full of jargon.'
+console.log(uff.includes('jargon'))
+
+let zzz = Math.random() * (100-50)+50;
+console.log(Math.floor (zzz))
+
+let zzx = Math.random()*255
+console.log(Math.floor(zzx))
+
+let javabomba = 'JavaScript'
+let risultatooo = Math.floor(Math.random()*(javabomba.length))
+console.log(risultatooo)
+console.log(javabomba[risultatooo])
+
+let porcoddue = "1 1 1 1 \n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125"
+console.log(porcoddue)
+
+let giga = 'You cannot end a sentence with because because because is a conjunction'
+let gigauno = giga.split(" ")
+gigauno.splice(6, 3)
+console.log(gigauno)
+
+let love = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+let gigus = love.split(' ')
+console.log(gigus)
+
+
+
+
