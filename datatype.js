30224f7367cc7b36480f9fd72b7858a713a69fd1
E-RKP <edobackup111@gmail.com>
2025-04-10 11:50:07 +0200

    versione 1

---
+++ datatype.js
@@ -0,0 +1,33 @@
+let spirulita = 'sei smonellita'
+console.log(spirulita)
+console.log(typeof spirulita)
+let number = 3
+console.log(typeof number)
+let float = 3.2
+console.log(typeof float)
+let ciao
+console.log(typeof ciao)
+let cio = null
+console.log(cio)
+let numeri = [0,2,4,7]
+console.log(typeof numeri)
+let fixed = 3.438927927902
+console.log(fixed.toFixed(6))
+let spirulta = 'ciao' + 'ciao'
+console.log(spirulta)
+const skills = [{}, {}, {}]
+const ga = '55'
+console.log(Number(ga)+3)
+
+let jj = 12;
+jj= jj-5;
+console.log(jj);
+
+let ju = 12;
+ju-=5;
+console.log(ju);
+
+let jk = 13;
+jk++;
+console.log(jk);
+
